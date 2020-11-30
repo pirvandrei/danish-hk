061edfbdf9c73f93adef36a54285fd0fdacbe7c9
Andrei Ilie Pîrvan <pirvanandrei@ymail.com>
2020-11-30 12:27:47 +0100

    uibuild

--- controllers/words.js
+++ controllers/words.js
@@ -1,7 +1,7 @@
 const wordsRouter = require('express').Router();
 const Word = require('../models/word');
-// const User = require('../models/user');
-// const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
 const getTokenFrom = (request) => {
   const authorization = request.get('authorization');
@@ -20,6 +20,41 @@ wordsRouter.get('/', (req, res) => {
       console.log('error connecting to MongoDB:', error.message);
       res.status(404).end();
     });
+});
+
+wordsRouter.post('/', async (request, response) => {
+  const body = request.body;
+
+  const token = getTokenFrom(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' });
+  }
+
+  const user = await User.findById(decodedToken.id);
+
+  if (body.lex === undefined) {
+    return response.status(400).json({
+      error: 'word missing',
+    });
+  }
+
+  const word = new Word({
+    lex: body.lex,
+    definitionDk: body.definitionDk,
+    definitionEn: body.definitionDk,
+    definitionRo: body.definitionDk,
+    createDate: new Date(),
+    updateDate: new Date(),
+    user: user._id,
+  });
+
+  const savedWord = await word.save();
+
+  // user.notes = user.notes.concat(savedNote._id);
+  // await user.save();
+
+  response.json(savedWord);
 });
 
 module.exports = wordsRouter;
