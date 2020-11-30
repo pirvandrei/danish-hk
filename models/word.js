803d2594b150c2a5bd8a76fd313dfc0cb4acc950
Andrei Ilie Pîrvan <pirvanandrei@ymail.com>
2020-11-30 09:13:39 +0100

    created the models

---
+++ models/word.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+mongoose.set('useFindAndModify', false);
+
+const wordSchema = new mongoose.Schema({
+  lex: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+});
+
+wordSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model('Word', wordSchema);
